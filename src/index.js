84da50272045ae7192a0419719492bb135357a75
Jared Suresky <jsuresky@gmail.com>
2020-11-18 22:33:02 -0500

    Some HTML added
    HTML added in server.js file and some tweeks in the index.html file.

---
+++ src/index.js
@@ -0,0 +1,53 @@
+import axios from "axios";
+
+const userList = document.querySelector("#user-list");
+const restaurantList = document.querySelector("#restaurants-list");
+const reservationsList = document.querySelector("#reservations-list");
+
+const renderUsers = (users) => {
+  const html = users
+    .map(
+      (user) => `
+    <li>
+        <a href='#${user.id}'>
+            ${user.name}
+        </a>
+    </li>
+    `
+    )
+    .join("");
+  userList.innerHTML = html;
+};
+
+const renderRestaurants = (restaurants) => {
+  const html = restaurants
+    .map(
+      (restaurant) => `
+      <li>
+        ${restaurant.name}
+      </li>
+      `
+    )
+    .join("");
+  reservationsList.innerHTML = html;
+};
+
+const init = async () => {
+  try {
+    const users = (await axios.get("/api/users")).data;
+    const restaurants = (await axios.get("/api/restaurants")).data;
+    renderUsers(users);
+    renderRestaurants(restaurants);
+  } catch (ex) {
+    console.log(ex);
+  }
+};
+
+window.addEventListener("hashchange", async () => {
+  const userId = window.location.hash.slice(1);
+  const url = `/api/users/${userId}/reservations`;
+  const reservations = (await axios(url)).data;
+  renderReservations(reservations);
+});
+
+init();
